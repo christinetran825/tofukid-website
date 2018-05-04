73110cbf7598a278ff635d9d3f0ba50a992ffd43
github username <github email address>
2018-05-04 13:29:50 -0700

    added images to illustrations component; added styling to illustrations

--- src/components/Illustrations.js
+++ src/components/Illustrations.js
@@ -6,24 +6,32 @@ class Illustrations extends Component {
       <section className="illustrations">
         <h1>Illustrations</h1>
         <div className="illustration">
-          <h4>Taste Addiction</h4>
+          <h3>Taste Addiction</h3>
           <p>I was playing with the idea of what kinds of 'dipping' or 'sauces' most people are addicted to. I expressed the gluttony of the addiction by having the source of ingredient drool and ooze its glorious goodness.</p>
+          <p>You can purchase this and other artworks at my <a href="https://society6.com/tofukidSociety6">Soceity6</a></p>
           <div className="images">
-            <p>Pictures Here</p>
+            <img src="../images/taste_all_sauces.jpg" alt="drooling sauces" />
+            <img src="../images/taste_ketchup.jpg" alt="drooling ketchup" />
+            <img src="../images/taste_peanut-butter.jpg" alt="drooling peanut butter" />
+            <img src="../images/taste_guacamole.jpg" alt="drooling avocado" />
           </div>
         </div>
         <div className="illustration">
           <h4>Thank You, Teachers</h4>
           <p>In honor of Teacher's Day, I've drawn a series of portraits of the teachers who inspired me in many ways throughout my education. Check out my Tumblr <a href="http://thanksforteachers.tumblr.com/" target="blank">"thanksforteacher"</a> to view more portraits.</p>
           <div className="images">
-            <p>Pictures Here</p>
+            <img src="../images/teachers-1.jpg" alt="Teachers Sketch" />
+            <img src="../images/teachers-2.jpg" alt="Teachers Sketch" />
+            <img src="../images/teachers-3.jpg" alt="Teachers Sketch" />
           </div>
         </div>
         <div className="illustration">
           <h4>Taco Time</h4>
           <p>Who doesn't like tacos? These digital art posters were made for an imaginary taco brand. Each poster has a pattern showing foods and objects related to tacos like chilies, hot sauce, sombreros.</p>
           <div className="images">
-            <p>Pictures Here</p>
+            <img src="../images/ortega-chili.jpg" alt="chili poster" />
+            <img src="../images/ortega-sauce.jpg" alt="sauce poster" />
+            <img src="../images/ortega-sombrero.jpg" alt="sombrero poster" />
           </div>
         </div>
       </section>
