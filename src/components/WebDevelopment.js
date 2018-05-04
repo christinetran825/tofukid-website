53c30c65302fc372e9c3bb69af6ad6b10cf6b6db
github username <github email address>
2018-05-04 12:25:25 -0700

    added strong to the details

--- src/components/WebDevelopment.js
+++ src/components/WebDevelopment.js
@@ -13,11 +13,12 @@ class WebDevelopment extends Component {
           <div className="project">
             <div className="title">
               <h2>SelfCare</h2>
-              <span className="fa-stack fa-lg fa-2x"><a href="https://github.com/christinetran825/SelfCare-C" target="_blank"><i className="fa fa-github"></i></a><a href="https://youtu.be/6Py4HcsAqNQ" target="_blank"><i className="fa fa-youtube"></i></a></span>
+              <span className="fa-stack fa-lg fa-1x"><a href="https://github.com/christinetran825/SelfCare-C" target="_blank"><i className="fa fa-github"></i></a><a href="https://youtu.be/6Py4HcsAqNQ" target="_blank"><i className="fa fa-youtube"></i></a></span>
             </div>
             <div className="details">
-              <p>Idea: There are a variety of habits that can affect mental health. From sleep to making sure medication was taken. Can we determine possible symptoms by tracking daily habits?</p>
-              <p>Challenge: Build a mobile health app for users to track their daily habits for a symptom management system. At the moment, how can I apply React & Redux to build the User Profile?</p>
+              <p><strong>Idea:</strong> There are a variety of habits that can affect mental health. From sleep to making sure medication was taken. Can we determine possible symptoms by tracking daily habits?</p>
+              <p><strong>Current Challenge:</strong> How can I apply React & Redux to build the User Profile?</p>
+              <p><strong>Major Challenge:</strong> Build a mobile health app for users to track their daily habits for a symptom management system.</p>
               <span>Note: The daily entry tracking is a work in progress.</span>
             </div>
             <div className="video">
@@ -28,11 +29,11 @@ class WebDevelopment extends Component {
           <div className="project">
             <div className="title">
               <h2>Artphilia</h2>
-              <span className="fa-stack fa-lg fa-2x"><a href="https://github.com/christinetran825/Artphilia" target="_blank"><i className="fa fa-github"></i></a><a href="https://youtu.be/LW-DTvgwrGs" target="_blank"><i className="fa fa-youtube"></i></a></span>
+              <span className="fa-stack fa-lg fa-x"><a href="https://github.com/christinetran825/Artphilia" target="_blank"><i className="fa fa-github"></i></a><a href="https://youtu.be/LW-DTvgwrGs" target="_blank"><i className="fa fa-youtube"></i></a></span>
             </div>
             <div className="details">
-              <p>Idea: There are plenty of artists (non-musicians) creating wonderful works of art. How can we keep a record of all the artists that we like?</p>
-              <p>Challenge: Create an app where users can create a library of artists (non-musicians) they like.</p>
+              <p><strong>Idea:</strong> There are plenty of artists (non-musicians) creating wonderful works of art. How can we keep a record of all the artists that we like?</p>
+              <p><strong>Challenge:</strong> Create an app where users can create a library of artists (non-musicians) they like.</p>
             </div>
             <div className="video">
               <p>put video</p>
@@ -45,9 +46,9 @@ class WebDevelopment extends Component {
               <span className="fa-stack fa-lg fa-2x"><a href="https://github.com/christinetran825/tea-crate" target="_blank"><i className="fa fa-github"></i></a><a href="https://youtu.be/3LCFISldE2w" target="_blank"><i className="fa fa-youtube"></i></a></span>
             </div>
             <div className="details">
-              <p>Idea: There are tea drinkers, then there are tea collectors. From a single tea leaf, tea has evolved into various types, flavors, and cultural influence.</p>
-              <p>Challenge: Design an app where a user can add a tea they've tried or want to try.</p>
-              <span>Side Challenge: Can we create a worldwide tea database?</span>
+              <p><strong>Idea:</strong> There are tea drinkers, then there are tea collectors. From a single tea leaf, tea has evolved into various types, flavors, and cultural influence.</p>
+              <p><strong>Challenge:</strong> Design an app where a user can add teas they've tried or will try.</p>
+              <span><strong>Side Challenge:</strong> Can we create a worldwide tea database?</span>
             </div>
             <div className="video">
               <p>put video</p>
@@ -60,8 +61,8 @@ class WebDevelopment extends Component {
               <span className="fa-stack fa-lg fa-2x"><a href="https://github.com/christinetran825/seed-picker-cli-app" target="_blank"><i className="fa fa-github"></i></a><a href="https://youtu.be/4QXM_g-vNr8" target="_blank"><i className="fa fa-youtube"></i></a></span>
             </div>
             <div className="details">
-              <p>Idea: I like vegetables. I chose a fun seed website to practice my CLI skills</p>
-              <p>Challenge: Build a CLI Gem using Nokogiri.</p>
+              <p><strong>Idea:</strong> I like vegetables. I chose a fun seed website to practice my CLI skills.</p>
+              <p><strong>Challenge:</strong> Build a CLI Gem using Nokogiri and Ruby.</p>
             </div>
             <div className="video">
               <p>put video</p>
