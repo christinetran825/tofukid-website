77d8f8ef6f5e9eda5a478140eb3437e3f8ff0d4e
github username <github email address>
2018-05-03 22:48:44 -0700

    updated div content to div about

--- src/components/About.js
+++ src/components/About.js
@@ -4,49 +4,46 @@ class About extends Component {
   render(){
     return (
       <div className="wrapper">
-        <div className="about">
-          <div className="hero">
-            <img src="../images/zukan.png" alt="Zukan Logo" width="200" height="200"/>
+        <div className="hero">
+          <img src="../images/zukan.png" alt="Zukan Logo" width="200" height="200"/>
+          <div className="cover-title">
             <h1>Christine Tran</h1>
             <span>Coder, Illustrator, Insight-seeker.</span>
           </div>
-          <div className="content">
-            <p>Full stack web developer with a passion for design, user experience, and data analysis. A lifelong learner who strives on building different skills. I view the world with a child-like awe, admiring the structures and pieces that make things whole by learning how to break things apart and putting them back together. I illustrate imaginative characters fit for children’s books in my spare time while sharing my curiosities of the world with trivial fun-facts and stories. On some days, you can find me at flea markets or estate sales hunting vintage paper, art, books, and teacups.</p>
-          </div>
+        </div>
+        <div className="about">
+          <p>Full stack web developer with a passion for design, user experience, and data analysis. A h4felong learner who strives on building different skills.</p>
+          <p>I view the world with a child-h4ke awe, admiring the structures and pieces that make things whole by learning how to break things apart and putting them back together. I illustrate imaginative characters fit for children’s books in my spare time while sharing my curiosities of the world with trivial fun-facts and stories. On some days, you can find me at flea markets or estate sales hunting vintage paper, art, books, and teacups.</p>
         </div>
 
         <section className="experiences">
           <div className="things-know">
             <h3>Things I Know</h3>
-            <ul>
-              <li className="experience">HTML</li>
-              <li className="experience">CSS</li>
-              <li className="experience">Javascript</li>
-              <li className="experience">Rails</li>
-              <li className="experience">Ruby</li>
-              <li className="experience">React</li>
-              <li className="experience">Redux</li>
-              <li className="experience">Illustrator</li>
-              <li className="experience">Photoshop</li>
-              <li className="experience">InDesign</li>
-              <li className="experience">User Interface/Experience</li>
-            </ul>
+            <h4 className="experience">HTML</h4>
+            <h4 className="experience">CSS</h4>
+            <h4 className="experience">Javascript</h4>
+            <h4 className="experience">Rails</h4>
+            <h4 className="experience">Ruby</h4>
+            <h4 className="experience">React</h4>
+            <h4 className="experience">Redux</h4>
+            <h4 className="experience">Illustrator</h4>
+            <h4 className="experience">Photoshop</h4>
+            <h4 className="experience">InDesign</h4>
+            <h4 className="experience">User Interface</h4>
+            <h4 className="experience">User Experience</h4>
           </div>
 
           <div className="things-do">
             <h3>Things I Can Do</h3>
-            <ul>
-              <li className="experience">Web Development/Design</li>
-              <li className="experience">Graphic Design</li>
-              <li className="experience">Visual Design</li>
-              <li className="experience">Illustration</li>
-              <li className="experience">Book Arts</li>
-              <li className="experience">Content</li>
-              <li className="experience">Strategy</li>
-            </ul>
+            <h4 className="experience">Web Design</h4>
+            <h4 className="experience">Graphic Design</h4>
+            <h4 className="experience">Visual Design</h4>
+            <h4 className="experience">Illustration</h4>
+            <h4 className="experience">Book Arts</h4>
+            <h4 className="experience">Content</h4>
+            <h4 className="experience">Strategy</h4>
           </div>
         </section>
-
       </div>
     )
   }
