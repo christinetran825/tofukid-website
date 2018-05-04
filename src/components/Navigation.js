f9faa62646a998270cf5d6986263b6185db06add
github username <github email address>
2018-05-03 17:40:01 -0700

    corrected root path to About component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,9 +4,8 @@ import { NavLink } from 'react-router-dom'
 class Navigation extends Component {
   render(){
     return (
-      <div className = 'navbar'>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About</NavLink>
+      <div className='navbar'>
+        <NavLink to="/">About</NavLink>
         <NavLink to="/web_development">Web Development</NavLink>
         <NavLink to="/illustrations">Illustrations</NavLink>
         <NavLink to="/bookmaking">Bookmaking</NavLink>
