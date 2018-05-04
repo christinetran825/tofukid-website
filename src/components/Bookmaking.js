dd5a32fca125b69a22d4e7d5d666159e4709d080
github username <github email address>
2018-05-04 12:35:43 -0700

    changed : to -; added p class of  notes for text of notes

--- src/components/Bookmaking.js
+++ src/components/Bookmaking.js
@@ -7,7 +7,7 @@ class Bookmaking extends Component {
         <h1>Book Arts</h1>
         <div className="book">
           <h4>Sending Hugs</h4>
-          <span>1st place MFA: Book Arts - AAU Spring Show 2013</span>
+          <p className="notes">1st place MFA: Book Arts - AAU Spring Show 2013</p>
           <p>One day my cousin sent her arm span of a hug by mail. Her sweet gift was a great idea for a children's book. "Sending Hugs" was meant to portray a bright orange colored muppet dressed in a long-sleeve dress shirt symbolizing the innocence and child-like gestures. My colophon (the author's information and comments) was placed on the back of the book as if it was the shirt tag sewn into the shirt. As the whole book expands, the text reads together fluidly from one arm to the other left-to-right AND right-to-left. Best of all, you can wrap the entire book around you for a hug.</p>
           <p>Pictures Here</p>
         </div>
@@ -33,7 +33,7 @@ class Bookmaking extends Component {
           <h4>Auspicious Hopes</h4>
           <p>Custom book with personal trinkets</p>
           <p>I've collected many red envelopes given to me during Chinese New Years and my birthday. Most cherished are the ones with my Chinese name written by my grandpa. I've also collected some fortune cookie slips during 2011-2012 when I had a year long fortune cookie craving. Many of the slips are poetic while others are humorous. Filled with hope, excitement, and love, I decided to create a book using the fortune slips to create my own poem that reflected my life at the time. I filled each envelope with old trinkets like dried flowers symbolizing each labeled fortune.</p>
-          <p>* Note: A slight design error: The title of my Chinese name should have been printed in red and gold, not white and black. White and black symbolizes the opposite of auspiciousness.</p>
+          <p className="notes">Note - A slight design error: The title of my Chinese name should have been printed in red and gold, not white and black. White and black symbolizes the opposite of auspiciousness.</p>
           <p>Pictures Here</p>
         </div>
       </section>
--- src/components/WebDevelopment.js
+++ src/components/WebDevelopment.js
@@ -16,10 +16,10 @@ class WebDevelopment extends Component {
               <span className="fa-stack fa-lg fa-1x"><a href="https://github.com/christinetran825/SelfCare-C" target="_blank"><i className="fa fa-github"></i></a><a href="https://youtu.be/6Py4HcsAqNQ" target="_blank"><i className="fa fa-youtube"></i></a></span>
             </div>
             <div className="details">
-              <p><strong>Idea:</strong> There are a variety of habits that can affect mental health. From sleep to making sure medication was taken. Can we determine possible symptoms by tracking daily habits?</p>
-              <p><strong>Current Challenge:</strong> How can I apply React & Redux to build the User Profile?</p>
-              <p><strong>Major Challenge:</strong> Build a mobile health app for users to track their daily habits for a symptom management system.</p>
-              <span>Note: The daily entry tracking is a work in progress.</span>
+              <p><strong>Idea -</strong> There are a variety of habits that can affect mental health. From sleep to making sure medication was taken. Can we determine possible symptoms by tracking daily habits?</p>
+              <p><strong>Current Challenge -</strong> How can I apply React & Redux to build the User Profile?</p>
+              <p><strong>Major Challenge -</strong> Build a mobile health app for users to track their daily habits for a symptom management system.</p>
+              <p className="notes">Note - The daily entry tracking is a work in progress.</p>
             </div>
             <div className="video">
               <p>put video</p>
@@ -32,8 +32,8 @@ class WebDevelopment extends Component {
               <span className="fa-stack fa-lg fa-x"><a href="https://github.com/christinetran825/Artphilia" target="_blank"><i className="fa fa-github"></i></a><a href="https://youtu.be/LW-DTvgwrGs" target="_blank"><i className="fa fa-youtube"></i></a></span>
             </div>
             <div className="details">
-              <p><strong>Idea:</strong> There are plenty of artists (non-musicians) creating wonderful works of art. How can we keep a record of all the artists that we like?</p>
-              <p><strong>Challenge:</strong> Create an app where users can create a library of artists (non-musicians) they like.</p>
+              <p><strong>Idea -</strong> There are plenty of artists (non-musicians) creating wonderful works of art. How can we keep a record of all the artists that we like?</p>
+              <p><strong>Challenge -</strong> Create an app where users can create a library of artists (non-musicians) they like.</p>
             </div>
             <div className="video">
               <p>put video</p>
@@ -46,9 +46,9 @@ class WebDevelopment extends Component {
               <span className="fa-stack fa-lg fa-2x"><a href="https://github.com/christinetran825/tea-crate" target="_blank"><i className="fa fa-github"></i></a><a href="https://youtu.be/3LCFISldE2w" target="_blank"><i className="fa fa-youtube"></i></a></span>
             </div>
             <div className="details">
-              <p><strong>Idea:</strong> There are tea drinkers, then there are tea collectors. From a single tea leaf, tea has evolved into various types, flavors, and cultural influence.</p>
-              <p><strong>Challenge:</strong> Design an app where a user can add teas they've tried or will try.</p>
-              <span><strong>Side Challenge:</strong> Can we create a worldwide tea database?</span>
+              <p><strong>Idea -</strong> There are tea drinkers, then there are tea collectors. From a single tea leaf, tea has evolved into various types, flavors, and cultural influence.</p>
+              <p><strong>Challenge -</strong> Design an app where a user can add teas they've tried or will try.</p>
+              <p><strong>Side Challenge -</strong> Can we create a worldwide tea database?</p>
             </div>
             <div className="video">
               <p>put video</p>
@@ -61,8 +61,8 @@ class WebDevelopment extends Component {
               <span className="fa-stack fa-lg fa-2x"><a href="https://github.com/christinetran825/seed-picker-cli-app" target="_blank"><i className="fa fa-github"></i></a><a href="https://youtu.be/4QXM_g-vNr8" target="_blank"><i className="fa fa-youtube"></i></a></span>
             </div>
             <div className="details">
-              <p><strong>Idea:</strong> I like vegetables. I chose a fun seed website to practice my CLI skills.</p>
-              <p><strong>Challenge:</strong> Build a CLI Gem using Nokogiri and Ruby.</p>
+              <p><strong>Idea -</strong> I like vegetables. I chose a fun seed website to practice my CLI skills.</p>
+              <p><strong>Challenge -</strong> Build a CLI Gem using Nokogiri and Ruby.</p>
             </div>
             <div className="video">
               <p>put video</p>
