50e5fe792f2465a3ec59e82704aae150d881931a
github username <github email address>
2018-05-03 18:25:56 -0700

    reordered social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,14 @@ const Footer = () =>
     <div id="icons">
       <ul>
         <li><span className="fa-stack fa-lg fa-1x"><a href="mailto:christinetran825@gmail.com"><i className="fa fa-envelope-o" aria-hidden="true"></i></a></span></li>
-        <li><span className="fa-stack fa-lg fa-1x"><a href="https://www.instagram.com/thetofukid/" target="_blank"><i className="fa fa-instagram" aria-hidden="true"></i></a></span></li>
+
+        <li><span className="fa-stack fa-lg fa-1x"><a href="https://github.com/christinetran825" target="_blank"><i className="fa fa-github"></i></a></span></li>
+
         <li><span className="fa-stack fa-lg fa-1x"><a href="https://www.linkedin.com/in/christinetran825/" target="_blank"><i className="fa fa-linkedin" aria-hidden="true"></i></a></span></li>
-        <li><a href="https://github.com/christinetran825">GitHub</a></li>
-        <li><a href="https://medium.com/@christinetran_81275">Medium Blog</a></li>
+
+        <li><span className="fa-stack fa-lg fa-1x"><a href="https://twitter.com/thetofukid" target="_blank"><i className="fa fa-twitter"></i></a></span></li>
+
+        <li><span className="fa-stack fa-lg fa-1x"><a href="https://www.instagram.com/thetofukid/" target="_blank"><i className="fa fa-instagram" aria-hidden="true"></i></a></span></li>
       </ul>
     </div>
   </footer>
