f6be5b29ab59867a34a692362f4f090d2f4398c8
github username <github email address>
2018-05-04 18:54:18 -0700

    moved About and WebDevelopment to containers

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Navigation from './components/Navigation';
-import About from './components/About';
-import WebDevelopment from './components/WebDevelopment';
+import About from './containers/About';
+import WebDevelopment from './containers/WebDevelopment';
 import ArtDesign from './containers/ArtDesign';
 import Footer from './components/Footer';
 import './App.css';
