1655b351fac70fa767ae0ff640ec51e400352533
github username <github email address>
2018-05-04 18:03:51 -0700

    changed web dev to web apps

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,7 @@ class Navigation extends Component {
     return (
       <div className='navbar'>
         <NavLink to="/">ABOUT</NavLink>
-        <NavLink to="/web_development">WEB DEVELOPMENT</NavLink>
+        <NavLink to="/web_development">WEB APPS</NavLink>
         <NavLink to="/art_design">ART + DESIGN</NavLink>
         <NavLink to="https://medium.com/@christinetran_81275" target="_blank">BLOG</NavLink>
       </div>
