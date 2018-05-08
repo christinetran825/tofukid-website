67ce82128bbd31133c1a7190b8eafb555f57d3da
github username <github email address>
2018-05-07 21:28:47 -0700

    cleaned up skills/experience

--- src/containers/About.js
+++ src/containers/About.js
@@ -10,50 +10,44 @@ class About extends Component {
             <h1>Christine Tran</h1>
             <span>Coder, Illustrator, Insight-seeker.</span>
           </div>
-        </div>
-        <div className="blurb">
-          <p>Full stack web developer with a passion for design, user experience, and data analysis. A lifelong learner who strives on building different skills.</p>
-          <p>I view the world with a child-like awe, admiring the structures and pieces that make things whole by learning how to break things apart and putting them back together.</p>
-          <p>I illustrate imaginative characters fit for children’s books in my spare time while sharing my curiosities of the world with trivial fun-facts and stories. On some days, you can find me at flea markets or estate sales hunting vintage paper, art, books, and teacups.</p>
+          <section className="blurb">
+            <p>Full stack web developer with a passion for design, user experience, and data analysis. A lifelong learner who strives on building different skills.</p>
+            <p>I view the world with a child-like awe, admiring the structures and pieces that make things whole by learning how to break things apart and putting them back together.</p>
+            <p>I illustrate imaginative characters fit for children’s books in my spare time while sharing my curiosities of the world with trivial fun-facts and stories. On some days, you can find me at flea markets or estate sales hunting vintage paper, art, books, and teacups.</p>
+          </section>
         </div>
 
         <section className="experiences">
-          <div className="experience">
-            <div className="things-know">
-              <h3>Things I Know</h3>
-              <h4 className="experience">HTML</h4>
-              <h4 className="experience">CSS</h4>
-              <h4 className="experience">Javascript</h4>
-              <h4 className="experience">Rails</h4>
-              <h4 className="experience">Ruby</h4>
-              <h4 className="experience">React</h4>
-              <h4 className="experience">Redux</h4>
-              <h4 className="experience">Illustrator</h4>
-              <h4 className="experience">Photoshop</h4>
-              <h4 className="experience">InDesign</h4>
-              <h4 className="experience">User Interface</h4>
-              <h4 className="experience">User Experience</h4>
-            </div>
+          <div className="experience know">
+            <h3>Things I Know</h3>
+            <h4>HTML</h4>
+            <h4>CSS</h4>
+            <h4>Javascript</h4>
+            <h4>Rails</h4>
+            <h4>Ruby</h4>
+            <h4>React</h4>
+            <h4>Redux</h4>
+            <h4>Illustrator</h4>
+            <h4>Photoshop</h4>
+            <h4>InDesign</h4>
+            <h4>User Interface</h4>
+            <h4>User Experience</h4>
           </div>
-          <div className="experience">
-            <div className="things-do">
-              <h3>Things I Can Do</h3>
-              <h4 className="experience">Web Design</h4>
-              <h4 className="experience">Graphic Design</h4>
-              <h4 className="experience">Visual Design</h4>
-              <h4 className="experience">Illustration</h4>
-              <h4 className="experience">Book Arts</h4>
-              <h4 className="experience">Content Strategy</h4>
-            </div>
+          <div className="experience do">
+            <h3>Things I Can Do</h3>
+            <h4>Web Design</h4>
+            <h4>Graphic Design</h4>
+            <h4>Visual Design</h4>
+            <h4>Illustration</h4>
+            <h4>Book Arts</h4>
+            <h4>Content Strategy</h4>
           </div>
-          <div className="experience">
-            <div className="things-like">
-              <h3>Things I Like</h3>
-              <h4 className="experience">Vintage Children's Books</h4>
-              <h4 className="experience"><a href="https://www.atlasobscura.com/">Atlas Obscura</a></h4>
-              <h4 className="experience">Photography</h4>
-              <h4 className="experience">Food</h4>
-            </div>
+          <div className="experience like">
+            <h3>Things I Like</h3>
+            <h4>Vintage Children's Books</h4>
+            <h4><a href="https://www.atlasobscura.com/">Atlas Obscura</a></h4>
+            <h4>Photography</h4>
+            <h4>Food</h4>
           </div>
         </section>
       </div>
