e7ef2aec48367ccf1cc94dffd40717c6f1acc077
github username <github email address>
2018-05-07 20:27:19 -0700

    rebuilt the grid with grid area names; added header div to wrap navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,11 +4,13 @@ import { NavLink } from 'react-router-dom'
 class Navigation extends Component {
   render(){
     return (
-      <div className='navbar'>
-        <NavLink to="/">ABOUT</NavLink>
-        <NavLink to="/web_development">WEB APPS</NavLink>
-        <NavLink to="/art_design">ART + DESIGN</NavLink>
-        <NavLink to="https://medium.com/@christinetran_81275" target="_blank">BLOG</NavLink>
+      <div className="header">
+        <nav className="navbar">
+          <NavLink to="/">ABOUT</NavLink>
+          <NavLink to="/web_development">WEB APPS</NavLink>
+          <NavLink to="/art_design">ART + DESIGN</NavLink>
+          <NavLink to="https://medium.com/@christinetran_81275" target="_blank">BLOG</NavLink>
+        </nav>
       </div>
     )
   }
