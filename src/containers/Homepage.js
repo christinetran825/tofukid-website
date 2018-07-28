867c7072dfb3605c24d4504a52bab01c1f0e8a0d
github username <github email address>
2018-07-27 18:21:18 -0700

    created a homepage

---
+++ src/containers/Homepage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+  render(){
+    return (
+      <div className="container">
+        <div className="home-title">
+          <h1>Christine Tran</h1>
+          <h5>The Tofu Kid</h5>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Homepage;
