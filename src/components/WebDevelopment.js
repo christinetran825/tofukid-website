8c115da4cbc3f8cf50a6c58e23efb3651ddee0e0
github username <github email address>
2018-07-27 18:26:24 -0700

    moved from containers to components to be called in Work container based upon state

---
+++ src/components/WebDevelopment.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import WebApps from './WebApps'
+
+class WebDevelopment extends Component {
+  render(){
+    return (
+      <section className="web-dev">
+        <WebApps/>
+      </section>
+    )
+  }
+}
+
+export default WebDevelopment;
