5d951331b3443717600046b6011afc0aed3ff6ff
github username <github email address>
2018-07-29 23:54:57 -0700

    moved WebApps to container; will add state to pass in respectived webapps projects

---
+++ src/containers/WebApps.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SelfCare extends Component {
+  render(){
+    return (
+      <section className="webapps">
+        <span><a href="/work">Back to Web Development</a></span>
+        {/* show webapps component */}
+      </section>
+    )
+  }
+}
+
+export default SelfCare;
