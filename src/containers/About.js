7a953ac0f4bfb3f54d75b06924dfd36f6b1c29ea
github username <github email address>
2018-08-05 19:35:26 -0700

    changed full-stack copy to say full0stack UX Designer; changed Message in Developer section.

--- src/containers/About.js
+++ src/containers/About.js
@@ -7,14 +7,14 @@ class About extends Component {
         <h1 className="page-title">Christine Tran</h1>
         <h2 className="sub-title">Synthesizer</h2>
         <div className="blurb">
-          <p>Full stack web developer with a passion for design, user experience, and data.</p>
-          <p>As an abstract  thinker, I’m fascinated by the future. I enjoy seeing the bigger picture while anticipating and  imagining in detail what tomorrow could or should be. Inspired by the adage of making the familiar strange and the strange familiar, I admire the structures and pieces that make things whole through a child-like awe. Art, design, and complex philosophical or scientific concepts are like catnip to me. Wondering, and making connections satisfies my need to see how a story ends. To feel and experience the gratitude and appreciation of people’s delight through making culture, not just consuming it fuels me.</p>
+          <p>Full-Stack UX Designer</p>
+          <p>As an abstract thinker, I’m fascinated by the future. I enjoy seeing the bigger picture while anticipating and  imagining in detail what tomorrow could or should be. Inspired by the adage of making the familiar strange and the strange familiar, I admire the structures and pieces that make things whole through a child-like awe. Art, design, and complex philosophical or scientific concepts are like catnip to me. Wondering, and making connections satisfies my need to see how a story ends. To feel and experience the gratitude and appreciation of people’s delight through making culture, not just consuming it fuels me.</p>
         </div>
         <section className="experiences">
           <div className="experience developer">
             <img src="../images/icons/004-user.png" alt="user diagram" />
             <h2>Developer</h2>
-            <p>When building with code, I am product focused while keeping endusers in mind.</p>
+            <p>Background in programming; Working with developers, as a designer with programming literacy makes process seamless.</p>
           </div>
           <div className="experience designer">
             <img src="../images/icons/003-gear.png" alt="gear in circle" />
