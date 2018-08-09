7884caa4e7de3a50a7916b9438b99c8ee72d1829
github username <github email address>
2018-08-09 11:36:43 -0700

    added images of projects to artphilia and tea-crate; added content

--- src/components/WebApps/Artphilia.js
+++ src/components/WebApps/Artphilia.js
@@ -16,10 +16,36 @@ class Artphilia extends Component {
             </div>
           </div>
           <div className="details">
-            <h4>IDEA</h4>
-            <p>There are plenty of artists (non-musicians) creating wonderful works of art. How can we keep a record of all the artists that we like?</p>
-            <h4>CURRENT CHALLENGE</h4>
-            <p>Create an app where users can create a library of artists (non-musicians) they like.</p>
+            <div className="roles">
+              <p>My Role: UX, UI, Branding, Developer</p>
+              <p>Platform: Web App</p>
+              <p>Languages & Frameworks: Ruby, Ruby on Rails, HTML, CSS, jQuery, Javascript</p>
+              <p>Tools: Adobe XD, Google Draw</p>
+            </div>
+            <div className="summary">
+              <h2>Creating Artphilia</h2>
+              <p>Artphilia is a fictional company seeking to share independent artists we like, their work, and their story.</p>
+            </div>
+            <div className="problem">
+              <h2>The Problem</h2>
+              <h4>1. What kind of artists are we limiting this to?</h4>
+              <p>There are many artists creating wonderful works of art. Many aren’t seen in museums or galleries. Most can be found at art fairs, convention events, or on social media like Instagram.</p>
+            </div>
+            <div className="solution">
+              <h2>The Solution</h2>
+              <p>Design an app allowing users to input an artist’s name, website/portfolio link, where they were discovered, rating, and notes. With the artist recorded, allow users to upload an image of the artwork they like, own, or wish to own.</p>
+            </div>
+            <div className="visuals">
+              <h2>Sitemapping</h2>
+              <img className="site-mapping" src="./images/web-dev/artphilia/artphilia-site-mapping.jpg" alt="artphilia-site-mapping"/>
+              <h2>Wireframing</h2>
+              <img className="wireframes" src="./images/web-dev/artphilia/all-artists.jpg" alt="artphilia-all-artists-page"/>
+              <img className="wireframes" src="./images/web-dev/artphilia/artist-page.jpg" alt="artphilia-artists-page"/>
+              <img className="wireframes" src="./images/web-dev/artphilia/favorite-artists.jpg" alt="artphilia-favorite-artists-page"/>
+              <img className="wireframes" src="./images/web-dev/artphilia/artworks-owned.jpg" alt="artphilia-artworks-owned-page"/>
+              <img className="forms" src="./images/web-dev/artphilia/add-artist-form-modal.jpg" alt="artphilia-add-artists-form"/>
+              <img className="forms" src="./images/web-dev/artphilia/add-artwork-form-modal.jpg" alt="artphilia-add-artworks-form"/>
+            </div>
           </div>
         </div>
       </section>
--- src/components/WebApps/TeaCrate.js
+++ src/components/WebApps/TeaCrate.js
@@ -16,12 +16,42 @@ class TeaCrate extends Component {
             </div>
           </div>
           <div className="details">
-            <h4>IDEA</h4>
-            <p>There are tea drinkers, then there are tea collectors. From a single tea leaf, tea has evolved into various types, flavors, and cultural influence.</p>
-            <h4>CURRENT CHALLENGE</h4>
-            <p>Design an app where a user can add teas they've tried or will try.</p>
-            <h4>END GOAL</h4>
-            <p>Can we create a worldwide tea database?</p>
+            <div className="roles">
+              <p>My Role: UX, UI, Branding, Developer</p>
+              <p>Platform: Web App</p>
+              <p>Languages & Frameworks: Ruby, Ruby on Rails, Sinatra, Model View Controller (MVC), HTML, CSS</p>
+            </div>
+            <div className="summary">
+              <h2>Creating Tea Crate</h2>
+              <p>Tea represents a culture, flavors, and reflection. How it’s collected, processed, practiced, and consume varies from culture to culture and person to person. Usually, you can tell the level of tea devotion by the drinker’s tea collection. A casual drinker will have maybe three types of tea. Tea Lovers can have five or seven. True tea connoisseurs will an entire cupboard bursting with blends and flavors for any and every tea occasion. Tea Crate is a fictional company seeking to collect teas of the world through the help of their users.</p>
+            </div>
+            <div className="problem">
+              <h2>The Problem</h2>
+              <h4>1. Tracking Teas</h4>
+              <p>All pure teas originate from the same plant species, Camellia Sinensis. Teas become a type because of different processing procedures. Typically there are 7 types of tea - black, green, Oolong, white, roobios, and herbal infusions. Many brands, big and small, produce and market their own version of each type of tea.</p>
+              <h4>2. Defining Tea Experiences</h4>
+              <p>There are people who like tea, who love tea, and who can’t function without at least one cup of tea a day. There may be some who might not care or be able to distinguish a tea’s characteristics before adding any other ingredients. Depending on the tea drinker, the “way of tea” varies.</p>
+              <ul>
+                <li>Should tea be consumed neat, straight?</li>
+                <li>Should a flavor or a combination of flavors like milk, cream, sugar, and lemon be added to tea?</li>
+                <li>Should tea leaves be loose or in a teabag?</li>
+                <li>Should the tea be brewed at the exact duration for the perfect flavor?</li>
+              </ul>
+            </div>
+            <div className="solution">
+              <h2>The Solution</h2>
+              <p>Design an app allowing users to input the brand name, tea name, and tea type to the tea they’ve tried. They can include the way they brewed and consumed their tea to record their version of the perfect tea. Tea crate can collect the various teas experienced by their users while their users can keep track of how they had their best cup of tea?</p>
+            </div>
+            <div className="visuals">
+              <h2>Sitemapping</h2>
+              <img src="./images/web-dev/tea-crate/tea-crate-site-mapping.jpg" alt="tea-crate-site-mapping"/>
+              <h2>Wireframing</h2>
+              <img className="wireframes" src="./images/web-dev/tea-crate/landing-page-2x.jpg" alt="tea-crate-landing-page"/>
+              <img className="wireframes" src="./images/web-dev/tea-crate/my-tea-crate-2x.jpg" alt="tea-crate-tea-list"/>
+              <img className="wireframes" src="./images/web-dev/tea-crate/tea-resources-web-pages-2x.jpg" alt="tea-crate-tea-resources-web-pages"/>
+              <img className="wireframes" src="./images/web-dev/tea-crate/brew-timer-2x.jpg" alt="tea-crate-brew-timer"/>
+              <img className="forms" src="./images/web-dev/tea-crate/add-tea-form-modal-2x.jpg" alt="tea-crate-add-tea-form"/>
+            </div>
           </div>
         </div>
       </section>
