a91a2e48f839ba411a25487ff7e5d4fd050c3351
github username <github email address>
2018-08-09 19:24:52 -0700

    removed old image still, added blank image placeholder and artphilia and tea crate still

--- src/components/WebDevelopment.js
+++ src/components/WebDevelopment.js
@@ -4,12 +4,10 @@ class WebDevelopment extends Component {
   render(){
     return (
       <section id="web-dev">
-        <div className="projects">
-          <a href="/selfcare"><img src="#" alt="selfcare-still"/></a>
-          <a href="/artphilia"><img src="../images/web-dev/artphilia-still.png" alt="artphilia-still"/></a>
-          <a href="/teacrate"><img src="../images/web-dev/teacrate-still.png" alt="tea-crate-still"/></a>
-          <a href="/seedpicker"><img src="#" alt="seed-picker-still"/></a>
-        </div>
+        <a href="/selfcare"><img src="../images/web-dev/blank-image-2x.jpg" alt="selfcare-still"/></a>
+        <a href="/artphilia"><img src="../images/web-dev/artphilia/artphilia-landing-page-high-2x.jpg" alt="artphilia-still"/></a>
+        <a href="/teacrate"><img src="../images/web-dev/tea-crate/tea-crate-landing-page-high-2x.jpg" alt="tea-crate-still"/></a>
+        <a href="/seedpicker"><img src="../images/web-dev/blank-image-2x.jpg" alt="seed-picker-still"/></a>
       </section>
     )
   }
