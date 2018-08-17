7acb4b4038747474a429c1fa84dac051aa6c3093
github username <github email address>
2018-08-17 00:09:45 -0700

    added images for artphilia and tea-crate

--- src/components/WebDevelopment.js
+++ src/components/WebDevelopment.js
@@ -8,14 +8,17 @@ class WebDevelopment extends Component {
           <a href="/selfcare"><img src="#" alt="selfcare-still"/></a>
           <h3>SelfCare</h3>
         </div>
+        
         <div className="project">
-          <a href="/artphilia"><img src="../images/web-dev/artphilia/artphilia-landing-page-high-2x.jpg" alt="artphilia-still"/></a>
+          <a href="/artphilia"><img src="../images/web-dev/artphilia/prototypes/artphilia-landing-page-high-2x.jpg" alt="artphilia-still"/></a>
           <h3>Artphilia</h3>
         </div>
+
         <div className="project">
-          <a href="/teacrate"><img src="../images/web-dev/tea-crate/tea-crate-landing-page-high-2x.jpg" alt="tea-crate-still"/></a>
+          <a href="/teacrate"><img src="../images/web-dev/tea-crate/prototypes/landing-page-high.jpg" alt="tea-crate-still"/></a>
           <h3>Tea Crate</h3>
         </div>
+
         <div className="project">
           <a href="/seedpicker"><img src="#" alt="seed-picker-still"/></a>
           <h3>Seed Picker</h3>
