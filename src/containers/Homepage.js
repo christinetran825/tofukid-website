9c9821aa7315237747e9c223cbeab9acc2b4772c
github username <github email address>
2018-08-17 00:09:06 -0700

    changed homepage class to id; moved h1 and h3 out of div

--- src/containers/Homepage.js
+++ src/containers/Homepage.js
@@ -4,16 +4,14 @@ import { Link } from 'react-router-dom'
 class Homepage extends Component {
   render(){
     return (
-      <main className="homepage">
-        <div className="home-title">
-          <h1>Christine Tran</h1>
-          <h5>The Tofu Kid</h5>
-        </div>
+      <main id="homepage">
+        <h1>Christine Tran</h1>
+        <h3>Full-Stack UX Designer Proficient in UXR</h3>
         <section className="latest-projects">
           <h2>Latest Projects</h2>
           <img src="#" alt="selfcare-still"/>
-          <img src="#" alt="artphilia-still"/>
-          <img src="#" alt="tea-crate-still"/>
+          <img src="../images/web-dev/artphilia/prototypes/artphilia-landing-page-high-2x.jpg" alt="artphilia-still"/>
+          <img src="../images/web-dev/tea-crate/prototypes/landing-page-high.jpg" alt="tea-crate-still"/>
           <Link to="/work">See More Work</Link>
         </section>
       </main>
