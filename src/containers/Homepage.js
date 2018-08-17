3cf25789d3d6d94c4ba90528bc7dbce6524d180f
github username <github email address>
2018-08-17 14:40:02 -0700

    moved view portfolio page to its own div

--- src/containers/Homepage.js
+++ src/containers/Homepage.js
@@ -12,8 +12,10 @@ class Homepage extends Component {
           <img src="#" alt="selfcare-still"/>
           <img src="../images/web-dev/artphilia/prototypes/artphilia-landing-page-high-2x.jpg" alt="artphilia-still"/>
           <img src="../images/web-dev/tea-crate/prototypes/landing-page-high.jpg" alt="tea-crate-still"/>
-          <Link to="/work">See More Work</Link>
         </section>
+        <div className="cta">
+          <Link to="/work" className="button" title="View Full Portfolio">View Full Portfolio</Link>
+        </div>
       </main>
     )
   }
