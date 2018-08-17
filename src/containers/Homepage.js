e5f371b8b1425c37e95fbb19829e85b3d4e728d8
github username <github email address>
2018-08-16 20:26:16 -0700

    added section for latest projects with link to work path; updated semantics

--- src/containers/Homepage.js
+++ src/containers/Homepage.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class Homepage extends Component {
   render(){
     return (
-      <div className="home-title">
-        <h1>Christine Tran</h1>
-        <h5>The Tofu Kid</h5>
-      </div>
+      <main className="homepage">
+        <div className="home-title">
+          <h1>Christine Tran</h1>
+          <h5>The Tofu Kid</h5>
+        </div>
+        <section className="latest-projects">
+          <h2>Latest Projects</h2>
+          <img src="#" alt="selfcare-still"/>
+          <img src="#" alt="artphilia-still"/>
+          <img src="#" alt="tea-crate-still"/>
+          <Link to="/work">See More Work</Link>
+        </section>
+      </main>
     )
   }
 }
