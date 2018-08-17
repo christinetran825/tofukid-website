f7cc832ccbe97bb6909326dbe3a45f1a5eda4680
github username <github email address>
2018-08-17 13:43:58 -0700

    adjusted span styles

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -6,9 +6,7 @@ class Contact extends Component {
       <main id="contact">
         <img src="./images/zukan.png" alt="tea cup ninja"/>
         <h1>Coffee or Tea?</h1>
-        <p><span>Send me a note and let’s build to learn.
-          <br/><br/>christinetran825@gmail.com</span><br/><br/>
-          I also like to illustrate imaginative characters fit for children’s books in my spare time while sharing my curiosities of the world with trivial fun-facts and stories. On some days, you can find me at flea markets or estate sales hunting for vintage paper, art, books, and teacups.</p>
+        <p><span>Send me a note and let’s build to learn</span> <br/><br/>christinetran825@gmail.com<br/><br/>I also like to illustrate imaginative characters fit for children’s books in my spare time while sharing my curiosities of the world with trivial fun-facts and stories. On some days, you can find me at flea markets or estate sales hunting for vintage paper, art, books, and teacups.</p>
       </main>
     )
   }
