d05e4de431d90761d1e38e3915a8d64a614cb448
github username <github email address>
2018-08-17 14:40:37 -0700

    moved logo to own div of site-branding; gave header an id

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,15 +4,21 @@ import { NavLink } from 'react-router-dom'
 class Navigation extends Component {
   render(){
     return (
-      <header>
+      <header id="masthead">
+        <div id="site-branding">
+          <h1 class="site-title">
+            <NavLink to="/" id="logo">Christine Tran</NavLink>
+          </h1>
+        </div>
         <nav className="menu">
-          <NavLink to="/" id="logo">Christine Tran</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/work">Work</NavLink>
-          <NavLink to="https://medium.com/@christine_tran" target="_blank">Blog</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
+          <ul>
+            <li><NavLink to="/about">About</NavLink></li>
+            <li><NavLink to="/work">Work</NavLink></li>
+            <li><NavLink to="https://medium.com/@christine_tran" target="_blank">Blog</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
           <button className="burger" aria-expanded="false" aria-controls="menu-list">
-            <span className="open"><i className="fa fa-bars" aria-hidden="true"></i></span>
+            <span className="open"><i className="fa fa-bars" aria-hidden="true"></i> Menu</span>
             <span className="close">x</span>
           </button>
         </nav>
