2324c6f534d4d9ffad6612adb2ae0b4b1dc59225
github username <github email address>
2018-08-22 13:51:22 -0700

    updated content

--- src/components/WebApps/SeedPicker.js
+++ src/components/WebApps/SeedPicker.js
@@ -27,7 +27,7 @@ class SeedPicker extends Component {
           <div className="summary">
             <h2>Developing Seed Picker</h2>
             <p>When I first learned how to code, I started with the basics - HTML and CSS. I wasn’t aware of any language beyond those two. And so, I made it a goal to learn new languages and functionalities of web design. This eventually lead me towards a Full-Stack program with Flatiron.</p>
-            <p>Built as one of my Flatiron School projects, my primary goal was to develop a Ruby Gem. The gem provides a Command Line Interface (CLI) to an external data source through the use of Nokogiri. The CLI is composed entirely as an Object Oriented Ruby application. We’re essentially stripping the entire external data source down to its data. No imagery, no color, no styles. Just simple text and numbers. And Nokogiri is perfect for this job.</p>
+            <p>Built as one of my Flatiron School projects, my primary goal was to develop a Ruby Gem. The gem provides a Command Line Interface (CLI) to an external data source through the use of Nokogiri. The CLI is composed entirely as an Object Oriented Ruby application. We’re essentially stripping the entire external data source down to its data. No imagery, no color, no styles. Just simple text and numbers. And Nokogiri was perfect for this job.</p>
             <p>The app can be tested and reviewed at:</p>
               <ul>
                 <li>
@@ -38,42 +38,49 @@ class SeedPicker extends Component {
                   <Link to="https://github.com/christinetran825/Artphilia" target="_blank">GitHub </Link></li>
               </ul>
 
+            <section className="details">
+              <div className="problem">
+                <h2>The Problem</h2>
+                <h4>1. Lack of Cohesive HTML Structure</h4>
+                <p>The external data source I chose was <Link to="https://www.rareseeds.com/" target="_blank">Baker Creek Heirloom Seeds Rare Seeds</Link>. I thought a seed catalogue would be a great way to play with data and learn Object Oriented Ruby. The website's HTML structure wasn't cohesive. Some web pages should've had a templatized design as they followed a patterned.</p>
+              </div>
+              <div className="solution">
+                <h2>The Solution</h2>
+                <h4>1. Determine Patterns</h4>
+                <p>With the gem, I chose to include only the company’s vegetable seeds catalogue due to time. To prepare the gem, I first reviewed the catalogues’ site mapping to determine patterns. I was seeking patterns in how a user can click on a parent seed and find the seed they’re looking for, with its details and price.</p>
+                <p>From inspecting the web pages, I’ve noted two user journey design patterns. See below for details and the User Journey visuals.</p>
+                <ul>
+                  <li>Pattern A: Takes User from list of vegetables to the Selected Parent Vegetable Seed. A User sees a description paragraph about the selected seed and an image gallery of all the Selected Parent Seed seeds. At this point, a user can select a seed for purchase.</li>
+                  <li>Pattern B: Takes User from list of vegetables to the Selected Parent Vegetable Seed. A User may see a description paragraph about the selected seed. A User also sees an image gallery of the Categories of the Selected Parent Seed. At this point, a user can select a Category. This action takes them to the Category page where the user may see a description along with an image gallery of the variety of seeds in that category. Upon the user's choice a user can select a seed for purchase and proceed to checkout or continue shopping.</li>
+                </ul>
+                <h4>2. Review HTML & CSS Structure and Patterns</h4>
+                <p>Pattern A worked for most of the parent seeds. Due to that, I had to accommodate a specific Nokogiri scrape for the seeds affected by Pattern B. See visual to see how I determined the pattern.</p>
+              </div>
+            </section>
+
             <section className="design-process">
-              <h2>Data Source</h2>
-              <p>The external data source I chose was <Link to="https://www.rareseeds.com/" target="_blank">Baker Creek Heirloom Seeds Rare Seeds</Link>. I thought a seed catalogue would be a great way to play with data and learn Object Oriented Ruby.</p>
-
               <h2>Sitemapping</h2>
-              <p>With the gem, I chose to include only the company’s vegetable seeds catalogue due to time. To prepare the gem, I first reviewed the catalogues’ site mapping to determine patterns. I was seeking patterns in how a user can click on a parent seed and find the seed they’re looking for, with its details and price. Here’s the pattern of the site mapping:</p>
-
               <div id="sitemap" className="visuals">
                 <img src="../../images/web-dev/seed-picker/seed-picker-site-mapping.jpg" alt="seedpicker-site-mapping"/>
               </div>
 
               <h2>User Journey</h2>
-              <p>From inspecting the web pages, I’ve noted two user journey design patterns.</p>
-
-              <h4>Pattern A</h4>
               <div id="journey" className="visuals">
                 <img src="../../images/web-dev/seed-picker/seed-picker-user-journey-a.jpg" alt="seedpicker-user-journey-a"/>
-              </div>
-
-              <h4>Pattern B</h4>
-              <div id="journey" className="visuals">
                 <img src="../../images/web-dev/seed-picker/seed-picker-user-journey-b.jpg" alt="seedpicker-user-journey-b"/>
               </div>
 
               <h2>Scraping</h2>
-              <p>The pattern worked for most of the parent seeds, however, I did come across rare breaks in the pattern to some seeds because of its HTML. Due to that, I had to accommodate a specific Nokogiri scrape for those rare seeds.</p>
-
               <div id="scrapping" className="visuals">
                 <img src="../../images/web-dev/seed-picker/seed-picker-scraping-structure.jpg" alt="seedpicker-scraping-structure"/>
               </div>
             </section>
 
-            <section className="solution">
-              <h2>CLI</h2>
+            <section id="cli">
+              <h2>CLI in Action</h2>
               <h4>Pattern A</h4>
               <embed src="../../videos/seed-picker-cli-pattern-a.mp4" autostart="false" height="500" width="500" />
+
               <h4>Pattern B</h4>
               <embed src="../../videos/seed-picker-cli-pattern-b.mp4" autostart="false" height="500" width="500" />
             </section>
