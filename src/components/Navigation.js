b415fc62a46a38f604fbdb89bd159bc6cf1681b3
github username <github email address>
2018-08-28 00:02:11 -0700

    updated state for burger menu; added styles for drop down menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom'
 
 class Navigation extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      click: false
+    };
+  }
+
+  toggleMenuButton = (e) => {
+    this.setState({
+      click: true
+    });
+  }
+
   render(){
     return (
       <header id="masthead">
@@ -13,10 +26,17 @@ class Navigation extends Component {
               <li><NavLink to="https://medium.com/@christine_tran" target="_blank">Blog</NavLink></li>
               <li><NavLink to="/contact">Contact</NavLink></li>
             </ul>
-            <button className="burger" aria-expanded="false" aria-controls="menu-list">
+            <button className="burger" aria-expanded="false" aria-controls="menu-list" onClick={() => this.toggleMenuButton()}>
               <span className="open"><i className="fa fa-bars" aria-hidden="true"></i> Menu</span>
-              <span className="close">x</span>
             </button>
+            {this.state.click ?
+              <div className="menu-dropdown">
+                <p><NavLink to="/work">Work</NavLink></p>
+                <p><NavLink to="https://medium.com/@christine_tran" target="_blank">Blog</NavLink></p>
+                <p><NavLink to="/contact">Contact</NavLink></p>
+              </div>
+             : null
+           }
           </nav>
         </div>
       </header>
